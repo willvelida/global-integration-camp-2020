528987d1205a44767fb4b7653a1786adf317c291
Will Velida <willvelida@hotmail.co.uk>
2020-02-06 12:15:41 +1300

    Adding core project and Product model

---
+++ GIBCosmosFunctionsDemo/GIDCosmos.Core/Models/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GIDCosmos.Core.Models
+{
+    public class Products
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public string ProductManufacturer { get; set; }
+        public string ProductType { get; set; }
+    }
+}
